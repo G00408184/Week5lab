19651d4cb74bfac17038ea61ec6697c2d2b6c2b7
Jakub Mamala <G00408184@atu.ie>
2023-10-24 16:19:13 +0100

    Finished the Feign Service and Feign Controller

---
+++ src/main/java/ie/atu/week5blockingapplication/FeignController.java
@@ -0,0 +1,36 @@
+package ie.atu.week5blockingapplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+public class FeignController {
+    private final FeignService feignService;
+
+    public FeignController(FeignService feignService){
+        this.feignService = feignService;
+    }
+
+    @GetMapping("/feign")
+    public String getFeignData() throws Exception, InterruptedException{
+        long startTime = System.currentTimeMillis();
+
+        List<CompletableFuture<TodoResponse>> futures = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++){
+            CompletableFuture<TodoResponse> future = CompletableFuture.supplyAsync(() -> feignService.fetchData());
+            futures.add(future);
+        }
+
+        CompletableFuture<Void> allof = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+
+        allof.get();
+        long endTime = System.currentTimeMillis();
+
+        return "Total execution time" + (endTime - startTime) + "ms";
+    }
+}
