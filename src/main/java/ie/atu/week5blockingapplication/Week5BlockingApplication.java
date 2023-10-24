d1d72839777d41f1d63958a3d650745b3366dd86
Jakub Mamala <G00408184@atu.ie>
2023-10-24 15:27:31 +0100

    created project

---
+++ src/main/java/ie/atu/week5blockingapplication/Week5BlockingApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week5blockingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5BlockingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5BlockingApplication.class, args);
+    }
+
+}
