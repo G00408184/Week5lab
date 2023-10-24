dbc7c8ef8c1d9702199fd17cc656f1a34184edeb
Jakub Mamala <G00408184@atu.ie>
2023-10-24 15:43:29 +0100

    crfinished the getblockingdata object

---
+++ src/main/java/ie/atu/week5blockingapplication/BlockingController.java
@@ -0,0 +1,23 @@
+package ie.atu.week5blockingapplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlockingController {
+    private final BlockingService blockingService;
+
+    public BlockingService getBlockingService() {
+        this.blockingService = blockingService;
+    }
+
+    @GetMapping("/blocking")
+    public String getBLockingData(){
+        long startTime = System.currentTimeMillis();
+        for (int i =0; i<10; i++){
+            blockingService.fetchDataBlocking();
+        }
+        long endTime = System.currentTimeMillis();
+        return "Total execution time" + (endTime-startTime) + "ms";
+    }
+}
